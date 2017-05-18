657b15f95eecd593d34a38d7b41c8091f4463d38
PRATHAP KUDUPU <PRATHAP KUDUPU>
2017-05-17 20:12:31 -0700

    Cart has been added.

--- Scripts/cartCntrl.js
+++ Scripts/cartCntrl.js
@@ -1,39 +1,42 @@
 var myModule=angular.module("myApp",[]);
 
 myModule.controller("cartCntrl",function($scope){
-    alert("hello");
-   $scope.cart=[];
    $scope.products=[
        {
-           productId:"1",
+           productCode:"121",
            productUrl:"http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
            productName:"Shoe",
-           productCode:"121",
-           productDesc:"Nike Shoes",
+           productDesc:"Garden broom",
            price:"50$"
        },
         {
-           productId:"2",
+           productCode:"122",
            productUrl:"http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png",
            productName:"Garden",
-           productCode:"122",
            productDesc:"Garden accessories",
            price:"60$"
        },
         {
-           productId:"3",
+           productCode:"123",
            productUrl:"http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png",
            productName:"Shoe",
-           productCode:"123",
            productDesc:"Garden Saw",
            price:"70$"
        }
      ];
-   
+    $scope.itemsAdded="";
+    $scope.filterGrid="";
+    $scope.cart=[];
+    
 $scope.add=function(pr){
-    //alert(items.productId);
-    cart.push(pr);
-    console.log("items"+pr);
-    alert("added to cart");
+
+   //Create the json object  
+    var items={
+        productId:pr.productCode,
+        productDesc:pr.productDesc,
+     };
+     $scope.cart.push(items);
+      //$scope.itemsAdded=pr.productDesc;
+   
 }
 });
